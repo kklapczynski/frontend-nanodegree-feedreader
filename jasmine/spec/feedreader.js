943598908d2e5c5f017406ffa2e139c4ce3566c5
Krzysztof Klapczyński <k.klapczynski@gmail.com>
2018-12-17 13:25:30 +0100

    tests: All TODO tests DONE

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -50,7 +50,7 @@ $(function() {
         })
     });
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* DONE: Write a new test suite named "The menu" */
     describe('The menu', () => {
         /* DONE: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -63,25 +63,75 @@ $(function() {
             expect(menuElement).toBeDefined();
             expect(menuElement.parentElement.classList.contains('menu-hidden')).toBe(true);
         })
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
-    });
-    /* TODO: Write a new test suite named "Initial Entries" */
+        /* DONE: Write a test that ensures the menu changes
+        * visibility when the menu icon is clicked. This test
+        * should have two expectations: does the menu display when
+        * clicked and does it hide when clicked again.
+        */
+        it('toggles when menu icon gets clicked', () => {
+            // using jQuery for tests: functions trigger, hasClass and others are jQuery functions
 
-        /* TODO: Write a test that ensures when the loadFeed
+            // function to trigger click event on menu icon
+            clickMenuIcon = () => {
+                $('.menu-icon-link').trigger('click');
+            }
+
+            clickMenuIcon();
+            // check if menu is visible: body doesn't have class 'menu-hidden'
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+            // trigger another click event on menu icon - menu should hide: bodu has a class 'menu-hidden'
+            clickMenuIcon();
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+
+    });
+
+    /* DONE: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', () => {
+        /* DONE: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach( (done) => {
+            loadFeed(0, done);
+        });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+        it('should have at least single entry element in .feed container', (done) => {
+            expect($('.feed').children()[0].classList.contains('entry-link')).toBe(true);
+            done();
+        })
+    });
 
+    /* DONE: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', () => {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        let firstHref, secondHref;
+        beforeEach( (done) => {
+            // load first feed ...
+            loadFeed(0, () => {
+                // this callback function is called only when async function loadFeed(0) returns success or error (see definition of loadFeed)
+                // ... and save its first entry href to compare with second feed
+                firstHref = $('.feed').children()[0].href;
+                // when first feed loadFeed call is finished (it is: either with success or error, cause this callback only then is called ),
+                // load second feed and save its first entry href
+                loadFeed(1, () => {
+                    secondHref = $('.feed').children()[0].href;
+                    // only here, when second feed is loaded we signal jasmine to check expectation
+                    done();
+                })
+            });
+        });
+
+        it('should change content displayed', (done) => {
+            expect(firstHref).toBeDefined();
+            expect(secondHref).toBeDefined();
+            expect(firstHref).not.toEqual(secondHref);
+            done();
+        })
+    });
 }());
